43cfa0aa9c866225e5289b5cf3b329dcd2858325
alexi <78235727+Alexialeonllopez@users.noreply.github.com>
2025-03-06 14:23:32 -0800

    added a .env file
    added a .env file that stores the CLIENT_ID and the SECRET_KEY for OAuth.

--- backend/src/main/java/com/team9/tierlist/TierlistApplication.java
+++ backend/src/main/java/com/team9/tierlist/TierlistApplication.java
@@ -3,11 +3,17 @@ package com.team9.tierlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class TierlistApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("OAUTH_CLIENT_ID", dotenv.get("OAUTH_CLIENT_ID"));
+        System.setProperty("OAUTH_CLIENT_SECRET", dotenv.get("OAUTH_CLIENT_SECRET"));
+		System.setProperty("PORT", dotenv.get("PORT"));
 		SpringApplication.run(TierlistApplication.class, args);
 	}
 }
